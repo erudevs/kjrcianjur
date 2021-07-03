ded7e3018af26007f526b81a557cb0b4c51d624c
ahmdhndr <achmadhendarsyah7@yahoo.com>
2021-07-03 17:01:52 +0700

    Finishing about page

--- public/script/script.js
+++ public/script/script.js
@@ -1,15 +1,18 @@
 const burger = document.querySelector('#burger');
 const close = document.querySelector('#close');
 const menu = document.querySelector('#menu');
+const body = document.querySelector('body');
+let opacity = 0;
+let intervalID = 0;
 
 burger.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
     menu.classList.remove('hidden');
-  } else {
-    menu.classList.add('hidden');
+    body.classList.replace('overflow-y-scroll', 'overflow-y-hidden');
   }
 });
 
 close.addEventListener('click', () => {
   menu.classList.add('hidden');
+  body.classList.replace('overflow-y-hidden', 'overflow-y-scroll');
 });

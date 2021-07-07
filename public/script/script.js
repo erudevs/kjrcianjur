0ce8645c1f279432db34c10122094134b011206c
ahmdhndr <achmadhendarsyah7@yahoo.com>
2021-07-07 19:55:01 +0700

    Remove reveal on scroll up effect

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('#burger');
 const close = document.querySelector('#close');
 const menu = document.querySelector('#menu');
+const menuNav = document.querySelector('#menuNav');
 const body = document.body;
 
 burger.addEventListener('click', () => {
@@ -17,22 +18,22 @@ close.addEventListener('click', () => {
 
 let lastScroll = 0;
 
-window.addEventListener('scroll', () => {
-  const currentScroll = window.pageYOffset;
+// window.addEventListener('scroll', () => {
+//   const currentScroll = window.pageYOffset;
 
-  if (currentScroll <= 0) {
-    body.classList.remove('scroll-up');
-  }
+//   if (currentScroll <= 0) {
+//     body.classList.remove('scroll-up');
+//   }
 
-  if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
-    body.classList.remove('scroll-up');
-    body.classList.add('scroll-down');
-  }
+//   if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+//     body.classList.remove('scroll-up');
+//     body.classList.add('scroll-down');
+//   }
 
-  if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
-    body.classList.remove('scroll-down');
-    body.classList.add('scroll-up');
-  }
+//   if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+//     body.classList.remove('scroll-down');
+//     body.classList.add('scroll-up');
+//   }
 
-  lastScroll = currentScroll;
-});
+//   lastScroll = currentScroll;
+// });

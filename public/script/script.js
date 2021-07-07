7511c7446c7d95d2dc482b0e37f0a845687463e6
ahmdhndr <achmadhendarsyah7@yahoo.com>
2021-07-07 18:46:17 +0700

    Add reveal on scroll-up navbar effect

--- public/script/script.js
+++ public/script/script.js
@@ -1,9 +1,7 @@
 const burger = document.querySelector('#burger');
 const close = document.querySelector('#close');
 const menu = document.querySelector('#menu');
-const body = document.querySelector('body');
-let opacity = 0;
-let intervalID = 0;
+const body = document.body;
 
 burger.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
@@ -15,4 +13,26 @@ burger.addEventListener('click', () => {
 close.addEventListener('click', () => {
   menu.classList.add('hidden');
   body.classList.replace('overflow-y-hidden', 'overflow-y-scroll');
+});
+
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    body.classList.remove('scroll-up');
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+    body.classList.remove('scroll-up');
+    body.classList.add('scroll-down');
+  }
+
+  if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+    body.classList.remove('scroll-down');
+    body.classList.add('scroll-up');
+  }
+
+  lastScroll = currentScroll;
 });

364c728defd0303ec936461d47193658cd3a22ca
ahmdhndr <achmadhendarsyah7@yahoo.com>
2021-07-12 18:00:47 +0700

    Add member page

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,6 @@
 const burger = document.querySelector('#burger');
 const close = document.querySelector('#close');
 const menu = document.querySelector('#menu');
-const menuNav = document.querySelector('#menuNav');
 const body = document.body;
 
 burger.addEventListener('click', () => {
@@ -15,25 +14,3 @@ close.addEventListener('click', () => {
   menu.classList.add('hidden');
   body.classList.replace('overflow-y-hidden', 'overflow-y-scroll');
 });
-
-let lastScroll = 0;
-
-// window.addEventListener('scroll', () => {
-//   const currentScroll = window.pageYOffset;
-
-//   if (currentScroll <= 0) {
-//     body.classList.remove('scroll-up');
-//   }
-
-//   if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
-//     body.classList.remove('scroll-up');
-//     body.classList.add('scroll-down');
-//   }
-
-//   if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
-//     body.classList.remove('scroll-down');
-//     body.classList.add('scroll-up');
-//   }
-
-//   lastScroll = currentScroll;
-// });

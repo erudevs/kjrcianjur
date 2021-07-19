609f001105fd26c920b4bb91ca9e1856f43f5b32
ahmdhndr <achmadhendarsyah7@yahoo.com>
2021-07-19 16:02:36 +0700

    Fix navbar and login/register modal

--- public/script/script.js
+++ public/script/script.js
@@ -1,14 +1,33 @@
 const burger = document.querySelector('#burger');
 const close = document.querySelector('#close');
 const menu = document.querySelector('#menu');
+const navItem = document.querySelectorAll('.nav-item a');
 const dropdownMenu = document.querySelector('#dropdownMenu');
 const overlay = document.querySelector('.overlay');
 const dropBtn = document.querySelector('.dropbtn');
 const dropdown = document.querySelector('.dropdown');
 const chevron = document.querySelector('.chevron');
-// let tabContent = document.querySelector('.tabcontent');
-// let tabLink = document.querySelector('.tablink');
-const body = document.body;
+const navLink = document.querySelectorAll('.nav-link');
+const modal = document.getElementById('modal');
+const loginModal = document.getElementById('loginModal');
+const registerModal = document.getElementById('registerModal');
+if (modal) {
+  if (loginModal) {
+    const closeModal = document.querySelector('.closeModal');
+    loginModal.onclick = function () {
+      modal.classList.add('show');
+    };
+    closeModal.onclick = function () {
+      modal.classList.remove('show');
+    };
+  }
+
+  window.onclick = function (e) {
+    if (e.target == modal) {
+      modal.classList.remove('show');
+    }
+  };
+}
 
 burger.addEventListener('click', () => {
   if (!menu.classList.contains('show')) {
@@ -17,20 +36,36 @@ burger.addEventListener('click', () => {
   }
 });
 
-close.addEventListener('click', () => {
+close.onclick = function () {
   if (menu.classList.contains('show')) {
     menu.classList.remove('show');
     overlay.style.display = 'none';
-    dropdownMenu.classList.remove('show');
+    if (dropdown) {
+      dropdownMenu.classList.remove('show');
+    }
   }
-});
+};
 
-dropBtn.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('show');
-});
+navLink.forEach((n) =>
+  n.addEventListener('click', () => {
+    if (menu.classList.contains('show')) {
+      menu.classList.remove('show');
+      overlay.style.display = 'none';
+      if (dropdown) {
+        dropdownMenu.classList.remove('show');
+      }
+    }
+  })
+);
+
+if (dropBtn) {
+  dropBtn.onclick = function () {
+    dropdownMenu.classList.toggle('show');
+  };
+}
 
 const openArticle = (e, articleName) => {
-  let i, tabContent, tabLink, tabHeader;
+  let i, tabContent, tabLink;
 
   tabContent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabContent.length; i++) {
